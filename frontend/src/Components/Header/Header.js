b7c59564e2b488bdb0350f79ff58fad7a2012bd5
PatOfCle <patofcle@gmail.com>
2023-09-07 13:43:55 -0400

    first commit

---
+++ frontend/src/Components/Header/Header.js
@@ -0,0 +1,22 @@
+import React from 'react'
+
+function Header() {
+  return (
+    <div>Header
+
+        <div className='navbar-container'>
+            <nav>
+                <ul>
+                <li><a href="/#">About</a></li>
+                <li><a href="/#">Product</a></li>
+                <li><a href="/#">Careers</a></li>
+                <li><a href="/#">Clients</a></li>
+                </ul>
+            </nav>
+        </div>
+
+    </div>
+  )
+}
+
+export default Header
