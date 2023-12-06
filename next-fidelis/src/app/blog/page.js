07422489036b065e12e6feac32dd69b457f3edcc
PatOfCle <patofcle@gmail.com>
2023-12-06 12:32:37 -0500

    Blog listings should now appear on Vercel.

--- next-fidelis/src/app/blog/page.js
+++ next-fidelis/src/app/blog/page.js
@@ -1,31 +1,77 @@
-"use client"
+// "use client"
 
-import React, {useState, useEffect} from 'react'
+// import React, {useState, useEffect} from 'react'
+import React from 'react'
 import client from './client'
 // import { Link } from 'react-router-dom'
 import './blog.css'
 import Link from 'next/link'
+import Homepage from './Homepage'
 
-function Blog() {
+// import { getAllPosts, getServerSideProps } from '../../../sanity/lib/api.js'
 
-    const [posts, setPosts] = useState([])
 
-    useEffect(() => {
-        client.fetch(
-            `*[_type == "post"] | order(_createdAt desc) {
-                title,
-                slug,
-                body,
-                mainImage {
-                    asset -> {
-                        _id,
-                        url
-                    },
-                    alt
-                }
-            }`
-        ).then((data) => setPosts(data)).catch(console.error)
-    }, [])
+async function getData() {
+    console.log('in first func')
+    // const res = await fetch('https://api.example.com/...')
+
+    const res = client.fetch(
+        `*[_type == "post"] | order(_createdAt desc) {
+            title,
+            slug,
+            body,
+            mainImage {
+                asset -> {
+                    _id,
+                    url
+                },
+                alt
+            }
+        }`
+    ).catch(console.error)
+
+    // The return value is *not* serialized
+    // You can return Date, Map, Set, etc.
+   
+    // if (!res.ok) {
+    //   // This will activate the closest `error.js` Error Boundary
+    //   throw new Error('Failed to fetch data')
+    // }
+   
+    // return res.json()
+    return res
+  }
+
+
+
+export default async function Blog() {
+
+    console.log('sup')
+
+    const posts = await getData()
+    console.log('data')
+    console.log(posts)
+
+
+    // const [posts, setPosts] = useState([])
+
+    // useEffect(() => {
+    //     client.fetch(
+    //         `*[_type == "post"] | order(_createdAt desc) {
+    //             title,
+    //             slug,
+    //             body,
+    //             mainImage {
+    //                 asset -> {
+    //                     _id,
+    //                     url
+    //                 },
+    //                 alt
+    //             }
+    //         }`
+    //     ).then((data) => setPosts(data)).catch(console.error)
+    // }, [])
+
 
   return (
     <div className='Blog-container'>
@@ -34,6 +80,10 @@ function Blog() {
         <h1 className='blog-page-header'>
             Knowledge Base
         </h1>
+
+        {/* <p>{data[0].title}</p> */}
+
+        {/* <Homepage /> */}
 
         <div className='blog-page-content'>
 
@@ -55,4 +105,4 @@ function Blog() {
   )
 }
 
-export default Blog
+// export default Blog
